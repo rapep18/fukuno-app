016a162bc86175e3304da09b1e2e4e9eead544a8
omsentod <rifqiwidyadana6@gmail.com>
2025-08-28 18:22:59 +0700

    Menambahkan file awal

---
+++ routes/api.php
@@ -0,0 +1,14 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use Illuminate\Http\Request; 
+use Illuminate\Support\Facades\Route;
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::get('/test', function () {
+    return response()->json(['message' => 'API works']);
+});
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
